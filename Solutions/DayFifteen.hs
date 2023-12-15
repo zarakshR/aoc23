7338a15f481951dd68df613bc32fa021537ba230
Zaraksh Rahman <zarakshR@pm.me>
2023-12-15 21:12:37 +0000

    map + map day fifteen

--- Solutions/DayFifteen.hs
+++ Solutions/DayFifteen.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE TupleSections #-}
-
 module Solutions.DayFifteen (input, partOne, partTwo) where
 
-import Prelude hiding (lookup)
-import Data.List.Split
-import Data.List
-import Data.Char
-import Data.Map hiding (foldl)
+import           Data.Char
+import           Data.List          (findIndex)
+import           Data.List.Split
+import           Prelude            hiding (lookup)
 
-import Text.Parsec
-import Text.Parsec.String
+import           Text.Parsec
+import           Text.Parsec.String (Parser)
+
+import qualified Data.Map           as M
+import           Data.Map.Ordered
+
 
 input :: String
--- input = "test_data"
-input = "inputs/15"
+input = "test_data"
+-- input = "inputs/15"
 
 hash = foldl hash' 0
        where
@@ -23,16 +25,18 @@ partOne :: String -> Int
 partOne = sum . fmap hash . splitOn "," . init
 
 data Operation = Set Int | Pop deriving (Show)
-data Step = Step String Operation deriving (Show)
+data Step = Step Int String Operation deriving (Show)
 
 parseHashmap :: String -> Either ParseError [Step]
 parseHashmap = parse hashmapP input
                where
                  hashmapP :: Parser [Step]
-                 hashmapP = stepP `sepBy1` (char ',')
+                 hashmapP = stepP `sepBy1` char ','
 
                  stepP :: Parser Step
-                 stepP = Step <$> many1 letter <*> operationP
+                 stepP = build <$> many1 letter <*> operationP
+                         where
+                           build label op = Step (hash label) label op
 
                  operationP :: Parser Operation
                  operationP = try popP <|> setP
@@ -41,35 +45,14 @@ parseHashmap = parse hashmapP input
                  popP = char '-' *> return Pop
 
                  setP :: Parser Operation
-                 setP = (Set . read) <$> (char '=' *> many1 digit)
+                 setP = Set . read <$> (char '=' *> many1 digit)
 
-type Boxes = Map Int [(String,Int)]
+initialize :: [Step] -> M.Map Int (OMap String Int)
+initialize = foldl (flip step) . M.fromList . fmap (,empty) $ [0..255]
 
-chug :: [Step] -> Boxes
-chug = foldl x (fromList . (fmap (,[])) $ [0..255])
-       where
-         x :: Boxes -> Step -> Boxes
-         x boxes (Step label Pop)
-            = adjust (deleteBy deleter (label,0)) (hash label) boxes
-              where
-                deleter (l,_) (l',_) = l == l'
-         x boxes (Step label (Set i))
-            = adjust (z (label,i)) (hash label) boxes
-              where
-                z :: (String,Int) -> [(String,Int)] -> [(String,Int)]
-                z (l,n) lenses = case Data.List.findIndex (\(x,y) -> x == l) lenses of
-                                    (Just i) -> let pref = Data.List.take i lenses
-                                                    suff = Data.List.drop (i + 1) lenses
-                                                in pref ++ [(l,n)] ++ suff
-                                    Nothing -> (l,n) : lenses
-                                 where
-                                   deleter (l,_) (l',_) = l == l'
+step :: Step -> M.Map Int (OMap String Int) -> M.Map Int (OMap String Int)
+step (Step hash label Pop)         = M.adjust (delete label) hash
+step (Step hash label (Set focus)) = M.adjust (alter (const $ Just focus) label) hash
 
 -- partTwo :: String -> Int
-partTwo = fmap (sum . concat . fmap x . toList . Data.Map.filter filt . chug) . parseHashmap
-          where
-            filt = not. Data.List.null
-
-            x (boxNo,lenses) = fmap (z boxNo) $ zip [1..] (fmap snd . reverse $ lenses)
-
-            z boxNo (lensNo,focalLen) = (boxNo + 1) * lensNo * focalLen
+partTwo = fmap initialize . parseHashmap
